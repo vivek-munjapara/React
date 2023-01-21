e06f4078587bcded3577177f375cb22274c4815e
Vivek <vivek.munjapara@gmail.com>
2023-01-21 14:22:20 +0530

    readmore complete

--- src/All_Task/ex_02_readmore/Card.js
+++ src/All_Task/ex_02_readmore/Card.js
@@ -4,8 +4,6 @@ import './Card.css';
 export default function Card() {
 
     const [data, setData] = useState([])
-    const [readMoreTitle, setReadMoreTitle] = useState(20)
-    const [readMore, setReadMore] = useState(50)
 
     useEffect(() => {
         fetch("https://fakestoreapi.com/products").then(y => y.json())
@@ -21,13 +19,24 @@ export default function Card() {
         setData(orgCard);
     }
 
-    const rdMoreTitle = (index) => {
+    // const rdMoreTitle = (index) => {
 
-        setReadMore(data[index].description.splice(0, 20));
-    }
-    const rdMore = (index) => {
+    //     // setReadMore(data[index].description.splice(0, 20));
+    // }
+    const rdMoreDetail = (index) => {
+        let v = document.getElementsByClassName('readMoreAnchor')[index].attributes.data;
+        console.log(v);
 
-        setReadMore(data[index].description.splice(0, 30));
+        if (v.value.includes("small")) {
+
+            document.getElementById(index).innerHTML = data[index].description;
+            v.value = "full";
+        }
+        else {
+            document.getElementById(index).innerHTML = data[index].description.slice(0, 50);
+            v.value = "small";
+
+        }
     }
 
     return (
@@ -36,19 +45,30 @@ export default function Card() {
             {
                 data.map((element, index) => {
                     return <div className='item'>
+
                         <div className='item-image'>
                             <div className='itemsub-image' >
                                 <img src={element.image} />
                             </div>
                         </div>
+
                         <div className='product-detail'>
-                            <div className='btm'><strong>Title:</strong> {element.title.slice(0, readMoreTitle)}<a onMouseOver={() => rdMoreTitle()}></a></div>
-                            <div className='btm'><strong>Price:</strong> {element.price}</div>
-                            <div className='btm'><strong>Category:</strong> {element.category}</div>
-                            <div className='btm'><strong>Description:</strong> {element.description.slice(0, readMore)}<a onClick={() => rdMore()}> Read More</a></div>
-                            <div className='dltbtn'>
-                                <button onClick={() => dltCard(index)}>Delete</button>
+                            <div className='btm'><strong>Title:</strong> {element.title}
+                                {/* <a onMouseOver={() => rdMoreTitle()}></a> */}
                             </div>
+
+                            <div className='btm'><strong>Price:</strong> {element.price}</div>
+
+                            <div className='btm'><strong>Category:</strong> {element.category}</div>
+
+                            <div className='btm'><strong>Description:</strong><span id={index}>{element.description.slice(0, 50)}</span><a data="small" className='readMoreAnchor' onClick={() => rdMoreDetail(index)}>...Read More</a></div>
+
+                            <div className='dltbtn'>
+                                <button onClick={() => dltCard(index)}>
+                                    <span>Delete</span>
+                                </button>
+                            </div>
+
                         </div>
                     </div>
                 })
