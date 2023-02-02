80c28309ff4fd18aa783836c996464116f0ec601
Vivek <vivek.munjapara@gmail.com>
2023-02-02 13:02:49 +0530

    Change in date field

--- src/All_Task/ex_06_dynamic_form/NormalDynamicForm.js
+++ src/All_Task/ex_06_dynamic_form/NormalDynamicForm.js
@@ -3,8 +3,17 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Box } from '@mui/system';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { FormControl } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 export default function DynamicNoramal() {
+
+    const eduLevel = ["Under Graduate", "Graduate", "Post Graduate", "PhD"];
 
     const [data, setData] = useState({
         basicInfo: {
@@ -104,13 +113,42 @@ export default function DynamicNoramal() {
                                     <TextField sx={{ width: '20%', mr: 1 }} name={`education.uniName[${index}]`} label='University' variant="outlined" onChange={myhandler} />
 
 
-                                    <TextField sx={{ width: '18%', mr: 1 }} type="date" name={`education.startDate[${index}]`} variant="outlined" onChange={myhandler} />
+                                    {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                        <DatePicker
+                                            label="Basic example"
+                                            name={`education.startDate[${index}]`}
+                                            value={data.education.startDate}
+                                            onChange={()=>myhandler}
+                                            renderInput={(params) => <TextField {...params} />}
+                                        />
+                                    </LocalizationProvider> */}
 
 
-                                    <TextField sx={{ width: '18%', mr: 1 }} type="date" name={`education.endDate[${index}]`} variant="outlined" onChange={myhandler} />
+                                    <TextField sx={{ width: '18%', mr: 1 }} type="date" name={`education.startDate[${index}]`} InputLabelProps={{ shrink: true }} label="Start Date" variant="outlined" onChange={myhandler} />
 
 
-                                    <TextField sx={{ width: '18%', mr: 1 }} name={`education.educationLevel[${index}]`} label='Level' variant="outlined" onChange={myhandler} />
+                                    <TextField sx={{ width: '18%', mr: 1 }} type="date" name={`education.endDate[${index}]`} InputLabelProps={{ shrink: true }} label="End Date" variant="outlined" onChange={myhandler} />
+
+
+                                    <FormControl sx={{ width: '18%', mr: 1 }}>
+                                        <InputLabel id="demo-simple-select-label ">Education Level</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-label"
+                                            id="demo-simple-select"
+                                            value={data.education.eduLevel}
+                                            label="Education Level"
+                                            onChange={myhandler}
+                                            name={`education.educationLevel[${index}]`}
+                                        >
+                                            {
+                                                eduLevel.map((edu) => {
+                                                    return (
+                                                        <MenuItem value={edu}>{edu}</MenuItem>)
+                                                })
+                                            }
+
+                                        </Select>
+                                    </FormControl>
 
                                     {
                                         index == 0 ? "" : <Button sx={{ p: 1, m: 1, width: '10%' }} type='button' variant="contained" startIcon={<DeleteIcon />} onClick={() => { deleteEducation(index) }}>Delete</Button>
